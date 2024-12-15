6ec458b345b5bb98f861ebbd43e19d38998a0d1f
sneikki <nvarvikko@gmail.com>
2024-12-15 09:09:07 +0200

    Set up socket io on server

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,3 +1,17 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+import socketio
 
-app = FastAPI()
+load_dotenv()
+
+from lib.td_client import start_api_conversation
+
+start_api_conversation()
+
+fastapi_app = FastAPI()
+socket = socketio.AsyncServer(cors_allowed_origins="*", async_mode="asgi")
+
+socket_app = socketio.ASGIApp(socket)
+fastapi_app.mount("/", socket_app)
+
+import lib.authentication
